58313788377f845286368fef82f5157eebe4db54
Ilya <rbetra@gmail.com>
2012-03-10 01:02:14 +0400

    add fabric deploy script and test functional

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+from fabric.api import *
+from os.path import join
+
+env.hosts = ['example.com']
+
+PROJECT_NAME = 'house_shop'
+PROJECT_DIR = join('/home/bezrukov/Envs/%s'%PROJECT_NAME, 'source')
+
+
+def run_in_virtualenv(command):
+    run('source %s/../bin/activate && %s' %
+        (PROJECT_DIR, command))
+
+def commit_to_hub():
+    message = raw_input("Enter a git commit message:  ")
+    local("git add . && git commit -m \"%s\"" % message)
+    local("git push origin master")
+
+def deploy():
+    with cd(PROJECT_DIR):
+        run('git pull')
+        run_in_virtualenv('pip install -r requirements.txt')
+        run_in_virtualenv('./manage.py syncdb')
+        run_in_virtualenv('./manage.py migrate')
+        #run_in_virtualenv('./manage.py clear_cache')
+        run('restart ' + PROJECT_NAME)
+
+
+#import settings
+#db_settings = settings.DATABASES['default']
+#
+#
+#def fetch_db():
+#    with cd(PROJECT_DIR):
+#        run('mysqldump %(NAME)s -u %(BACKUP_USER)s -p%(BACKUP_PASSWORD)s '
+#            '--skip-lock-tables > dump.sql' % db_settings)
+#        get('dump.sql', 'dump.sql')
+#    local('./manage.py flush --noinput')
+#    local('./manage.py dbshell < dump.sql')
